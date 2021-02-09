7b5e2e78df766cfb31185e1c489615d557d6683f
Kim Alvarez <kim.alvarez.scott@gmail.com>
2021-02-09 18:40:46 +0100

    Boton cambia de color

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 function ControlInputs(){
     if ((document.forms['frmConversor'].RGB.value ==="")&& (document.forms['frmConversor'].HEX.value==="")){
-        alert("Both fields empty!");
+        alert("Both fields empty.\nNo conversion will be done.");
     }else if (document.forms['frmConversor'].RGB.value ===""){
         alert("RGB empty! HEX IS "+ document.forms['frmConversor'].HEX.value );
     }else if(document.forms['frmConversor'].HEX.value===""){
         alert("HEX empty!")
+    }
+}
+function cambiaBoton(){
+    //Cambia el color de fondo del boton dependiendo de si alguno de los input texts tiene contenido o no.
+    if ((document.forms['frmConversor'].RGB.value !=="") || (document.forms['frmConversor'].HEX.value!=="")){
+        document.forms['frmConversor'].boton.style.backgroundColor= "#52b788";
+    }else if ((document.forms['frmConversor'].RGB.value ==="") && (document.forms['frmConversor'].HEX.value ==="")){
+        document.forms['frmConversor'].boton.style.backgroundColor= "#e56b6f";
     }
 }
 function Convertir(){
