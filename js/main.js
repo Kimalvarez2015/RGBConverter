f4ea88f4589bdf4ebfb03566aa78e24e6d176903
Kim Alvarez <kim.alvarez.scott@gmail.com>
2021-02-10 18:13:19 +0100

    Funcionalidad RGB->HEX completada con tarjeta de color!

--- js/main.js
+++ js/main.js
@@ -25,11 +25,25 @@ function vaciaHEX(){
 }
 function convertirValores(){
     controlInputs();
-    if(document.forms['frmConversor'].HEX.value===""){
+    if((document.forms['frmConversor'].HEX.value ==="") && (document.forms['frmConversor'].RGB.value !=="")){
         //ESTE ES EL IF QUE COMPRUEBA EL VALOR DE HEX VACIO PARA TRABAJAR EN RGB
         var RGB = document.forms['frmConversor'].RGB.value;
-        document.forms['frmConversor'].HEX.value=RGB;
-    }else if (document.forms['frmConversor'].RGB.value ===""){
+        var redRGB = Number(RGB.substr(0,3));
+        var greenRGB = Number(RGB.substr(4,3));
+        var blueRGB = Number(RGB.substr(8,3));
+        //ASIGNACION DE VARIABLES POR SEPARADO PARA OPERAR CON ELLAS. FUNCION NUMBER CONVIERTE EN NUMERO.
+        redRGB = redRGB.toString(16);
+        greenRGB = greenRGB.toString(16);
+        blueRGB = blueRGB.toString(16);
+        //FUNCION TOSTRING PARA CONVERTIR VALORES A HEXADECIMAL
+        document.forms['frmConversor'].HEX.value=("#"+redRGB+greenRGB+blueRGB);
+        //DAMOS VALOR AL INPUT EN HEX
+        document.getElementById('tarjetaColorCSS').style.visibility= "visible";
+        document.getElementById('colorTarjeta').style.background = document.forms['frmConversor'].HEX.value;
+        redRGB = 0;
+        greenRGB = 0;
+        blueRGB = 0;
+    }else if ((document.forms['frmConversor'].RGB.value ==="") && (document.forms['frmConversor'].HEX.value !=="")){
         //ESTE ES EL IF QUE COMPRUEBA EL VALOR DE RGB VACIO PARA TRABAJAR EN HEX
         var HEX = document.forms['frmConversor'].HEX.value;
         document.forms['frmConversor'].RGB.value=HEX;
